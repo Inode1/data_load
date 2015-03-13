9f498b0d3e70bd58c2d9cf313856115728eec812
ivan <ivan@ivan-P5K-SE.(none)>
2015-03-13 19:59:29 +0300

    Package modified

--- data_load.cpp
+++ data_load.cpp
@@ -2,26 +2,30 @@
  *
  */
 
-#include <iostream>
-
-#include <boost/iostreams/device/file_descriptor.hpp>
-#include <boost/iostreams/stream.hpp>
-#include <boost/filesystem.hpp>
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 
-#include "data_load.h"
+#include <iostream>
 
+#include <boost/iostreams/device/file_descriptor.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <boost/iostreams/device/mapped_file.hpp>
+#include <boost/iostreams/stream_buffer.hpp>
+#include <boost/regex.hpp>
+
+#include "data_load.h"
 
 using namespace std;
 using namespace boost::filesystem;
+using namespace boost::iostreams;
+
 const char* DataLoad::util[] = { "md5sum ",
                                  "sha1sum ",
                                  "sha256sum "
                                };
 const string DataLoad::m_ubuntuArchive = "ru.archive.ubuntu.com/ubuntu/dists/";
+
 DataLoad::DataLoad(const string &packageName):
         m_packageName(packageName)
 {
@@ -40,7 +44,7 @@ bool DataLoad::Load()
     return true;
 }
 
-bool DataLoad::GetFileInfo(const string &fileName, DataLoad::packageInfo &packageData, DataLoad::EPackageInfo info)
+bool DataLoad::GetFileInfo(const string &fileName, packageInfo &packageData, EPackageInfo info)
 {
     if (info == eAllField)
     {
@@ -66,7 +70,6 @@ bool DataLoad::GetFileInfo(const string &fileName, DataLoad::packageInfo &packag
             return true;
         }
     }
-
 
     string command = string{DataLoad::util[info]} + fileName;
     FILE *fp;
@@ -111,20 +114,133 @@ DataLoad::packageInfo DataLoad::GetPackageInfo()
 {
     return m_packageInfo;
 }
-void DataLoad::SetPackageInfo(const DataLoad::packageInfo &packageData)
+void DataLoad::SetPackageInfo(const packageInfo &packageData)
 {
     m_packageInfo = packageData;
 }
+bool DataLoad::ChangePackageFile(const boost::filesystem::path &packagePath, packageMap &result)
+{
+    string command;
+    cout << packagePath << " and " << packagePath.extension() << endl;
+    if (packagePath.extension().string() == ".bz2")
+    {
+        command = string("bunzip2 -f > /dev/null 2>&1 ") + packagePath.string();
+    }
+    else if (packagePath.extension().string() == ".gz")
+    {
+        command = string("gunzip -f > /dev/null 2>&1 ") + packagePath.string();
+    }
+    else
+    {
+        return false;
+    }
 
+    cout << "Unzip:" << packagePath << " Start" << endl;
+    if (system(command.c_str()))
+    {
+        cout << "Modified:" << packagePath << " Failed" << endl;
+        return false;
+    }
 
+    cout << "Unzip:" << packagePath << " Finished" << endl;
+
+    path filePath(packagePath.parent_path() / "Packages");
+    cout << filePath << endl;
+    if (!exists(filePath))
+    {
+        cout << filePath << " not exist" << endl;
+        return false;
+    }
+    mapped_file mapPackege(filePath.string());
+    stream<mapped_file> is(mapPackege, std::ios_base::binary);
+    string line;
+
+    while(getline(is, line))
+    {
+        if (line.find(string{"Package: "} + m_packageName) != std::string::npos)
+        {
+            cout << "Find" << endl;
+            boost::regex findString("^Size: .*");
+            while(getline(is, line))
+            {
+                if (boost::regex_match(line, findString))
+                {
+                    is.seekp(-(line.length() + 1), ios_base::cur);
+                    // check then length insert the same
+                    string size = string{"Size: "} + get<eSize>(m_packageInfo);
+                    int diff = line.length() - size.length();
+                    is << size                                         << endl;
+                    is << "MD5sum: " << get<eMD5Sum>(m_packageInfo)    << endl;
+                    is << "SHA1: "   << get<eSHA1Sum>(m_packageInfo)   << endl;
+                    int length;
+                    if (diff)
+                    {
+                        length = is.tellg();
+                        getline(is, line);
+                        getline(is, line);
+                        if (diff > 0)
+                        {
+                            getline(is, line);
+                        }
+                        cout << line << endl;
+                        is.seekp(length, ios_base::beg);
+
+                    }
+                    is << "SHA256: " << get<eSHA256Sum>(m_packageInfo) << endl;
+                    cout << diff << endl;
+                    if (diff > 0)
+                    {
+                        is << line << string(diff, '!') << endl;
+                    }
+                    else if (diff < 0)
+                    {
+                        is << line.substr(0, line.length() + diff) << endl;
+                    }
+
+/*                    if (diff > 0)
+                    {
+                        length = is.tellg();
+                        getline(is, line);
+                        getline(is, line);
+                        cout << line << endl;
+                        is.seekp(length, ios_base::beg);
+                        is << line << string(diff, '!') << endl;
+
+                     //       is << line << string(diff, '!');
+                    }*/
+
+                    cout << "Package:" << m_packageName << " modified in " <<  packagePath.parent_path()
+                         << " directory" << endl;
+                    break;
+                }
+            }
+            break;
+        }
+    }
+
+    is.~stream<mapped_file>();
+    mapPackege.~mapped_file();
+    command = string("bzip2 -f -k > /dev/null 2>&1 ") + filePath.string();
+    system(command.c_str());
+    command = string("gzip -f > /dev/null 2>&1 ") +  filePath.string();
+    system(command.c_str());
+    return true;
+}
+bool DataLoad::ChangeReleaseFile(const packageMap &result)
+{
+    return true;
+}
 int main()
 {
-    DataLoad data("ru.archive.ubuntu.com/ubuntu/dists/");
+    DataLoad data("account-plugin-aim");
    //data.Load();
 
     DataLoad::packageInfo d;
-    DataLoad::GetFileInfo("data_load.cpp", d);
-    cout << (std::get<1>(d)) << endl;
+    DataLoad::GetFileInfo("account-plugin-aim", d);
+    data.SetPackageInfo(d);
+    DataLoad::packageMap result;
+    data.ChangePackageFile(path("Packages.bz2"), result);
+    //cout << (get<1>(d)) << endl;
 
 
 
--- data_load.h
+++ data_load.h
@@ -8,6 +8,9 @@
 
 #include <string>
 #include <tuple>
+#include <map>
+
+#include <boost/filesystem.hpp>
 
 class DataLoad {
 public:
@@ -19,25 +22,29 @@ public:
         eSize,
         eAllField
     };
-
     typedef std::tuple<std::string, std::string,
                        std::string, std::string> packageInfo;
+    typedef std::map<boost::filesystem::path, packageInfo> packageMap;
     DataLoad(const std::string &packageName);
-	// Get ubuntu repo directory with
-	// Release and Package* File
-	bool Load();
-	// Package size and hash (in md5sun, sha1, sha256)
+    // Get ubuntu repo directory with
+    // Release and Package.* File
+    bool Load();
+    // Package size and hash (in md5sun, sha1, sha256)
 	static bool GetFileInfo(const std::string &fileName, packageInfo &packageData,EPackageInfo info = eAllField);
 	void SetPackageInfo(const packageInfo &packageData);
 	packageInfo GetPackageInfo();
+	// change Package.gz and Package.bz2
+    bool ChangePackageFile(const boost::filesystem::path &packagePath, packageMap &result);
+    // change main Release file
+    bool ChangeReleaseFile(const packageMap &result);
 
 	//void MakeReplacement();
-	~DataLoad()   = default;
+    ~DataLoad()   = default;
 private:
-	static const char* util[];
-	static const std::string       m_ubuntuArchive;
-	packageInfo                    m_packageInfo;
-	std::string                    m_packageName;
+    static const char* util[];
+    static const std::string       m_ubuntuArchive;
+    packageInfo                    m_packageInfo;
+    std::string                    m_packageName;
 };
 
 #endif /* DATA_LOAD_H_ */
